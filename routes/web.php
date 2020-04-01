4866b28e51bab4fbc3fe597d0ca3624ffddfb515
jfgils02 <41213909+jfgils02@users.noreply.github.com>
2020-04-01 13:30:55 -0600

    cambios a clientes

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['middleware' => ['auth','admin']], function () {
     Route::put('projects/economicAdvance/edit', 'Projects\EconomicAdvanceController@edit');
     Route::put('projects/technicalAdvance/edit', 'Projects\EconomicAdvanceController@edit');
     Route::post('upload', 'Projects/ImageController@save')->name('upload-post');
-    Route::get('users', 'UserController@showUsers');
+    Route::get('users', 'Users\UserController@showUsers');
     
 
     /*Route::get('customers', 'UserController@showCustomers');
