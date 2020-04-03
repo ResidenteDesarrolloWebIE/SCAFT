91534569a0b1844a8d797557058e72f5dae93dcb
ResidenteDesarrolloWebIE <residente.desarrolloweb@integracion-energia.com>
2020-04-03 12:39:45 -0600

    Diseño de usuarios

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -1,55 +1,58 @@
 @extends('layouts.app')
 @section('content')
 
+<head>
+    <link href="/css/user.css" rel="stylesheet">
+</head>
+
 @section('content')
 <section class="section-projects-admin py-2 text-xs-center">
-    @include('layouts.partials._navigationBar')
-    <div class="container container-projects-admin">
-        <div class="row table-responsive text-center projects-table">
-            <h1 class="text-center">Lista de Usuarios</h1>
-            <div class="offset-md-8 col-md-4 text-right">
-                <a data-toggle="modal" data-target="#createUser">
-                    <button id="btnUser" type="button" class="btn btn-success">
-                        Crear nuevo usuario<i class="fas fa-exchange-alt"></i>
-                    </button>
-                </a>
+    <div class="fondo">
+        @include('layouts.partials._navigationBar')
+        <div class="container container-projects-admin">
+            <div class="row table-responsive text-center projects-table">
+                <h1 class="text-center">Lista de Usuarios</h1>
+                <div class="offset-md-8 col-md-4 text-right">
+                    <a data-toggle="modal" data-target="#createUser">
+                        <button id="btnUser" type="button" class="btn btn-dark">
+                            Crear nuevo usuario<i class="fas fa-exchange-alt"></i>
+                        </button>
+                    </a>
+                </div>
+                <br>
+                <table class="table text-center table-sm-responsive" id="tableUsers">
+                    <thead style="background-color: #252b37">
+                        <tr>
+                            <th> Id</th>
+                            <th> Codigo</th>
+                            <th> Nombre</th>
+                            <th> E-mail</th>
+                            <th class="col-md-3">
+                                Acciones
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->code}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>
+                                <a data-toggle="modal" data-target="#editUser">
+                                    <button type="button" class="btn btn-primary" title="Editar Usuario"><i class="fas fa-edit"></i></button>
+                                </a>
+                            </td>
+                        </tr>
+                        @empty
+                        <p>No hay datos</p>
+                        @endforelse
+                    </tbody>
+                </table>
             </div>
-            <br>
-            <table class="table text-center table-sm-responsive" id="tableUsers">
-                <thead style="background-color:gray">
-                    <tr>
-                        <th> Id</th>
-                        <th> Codigo</th>
-                        <th> Nombre</th>
-                        <th> E-mail</th>
-                        <th class="col-md-3">
-                            Acciones
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($users as $user)
-                    <tr>
-                        <td>{{$user->id}}</td>
-                        <td>{{$user->code}}</td>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>
-                            <a data-toggle="modal" data-target="#editUser" >
-                                <button type="button" class="btn btn-primary"  title="Editar Usuario" ><i class="fas fa-edit"></i></button>
-                            </a>
-                        </td>
-                    </tr>
-                    @empty
-                    <p>No hay datos</p>
-                    @endforelse
-                </tbody>
-            </table>
         </div>
-    </div>
 
-    {{--@include('admin/users/create')--}}
-
-   
+        {{--@include('admin/users/create')--}}
 </section>
 @endsection
