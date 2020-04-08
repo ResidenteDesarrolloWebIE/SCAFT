6eaee4190be95600b8f3f75b6a68ef9d7432d70c
Israel-IE <israel.santiago@integracion-energia.com>
2020-04-08 12:39:50 -0500

    download document

--- app/Http/Controllers/Projects/OfferController.php
+++ app/Http/Controllers/Projects/OfferController.php
@@ -12,7 +12,8 @@ class OfferController extends Controller
     public function download(Request $request, $id){
         try {
             $project = Project::with('offer')->where('id', $id)->first();
-            return Storage::download($project->offer->file->path);
+            $url =storage_path("app/public/".$project->offer->file->path);
+            return response()->download($url,$project->offer->file->name);
         } catch (\Throwable $error) {
             echo("El error es : ".$error);
         }
